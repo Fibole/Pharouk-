80093dcbbfe93e8aa976ddd8b5e632da7e4ad784
Fibole <168233000+Fibole@users.noreply.github.com>
2024-05-04 22:43:49 +0000

    Update top.js

--- scripts/cmds/top.js
+++ scripts/cmds/top.js
@@ -20,11 +20,12 @@ module.exports = {
  
  const topUsers = allUsers.sort((a, b) => b.money - a.money).slice(0,10);
  
- const topUsersList = topUsers.map((user, index) => `${index + 1}.  ✦${user.name}✦:𝗕𝗔𝗟: ${user.money}💲`);
+ const topUsersList = topUsers.map((user, index) => `${index + 1}. 𝗠𝗔𝗦𝗧𝗘𝗥 : ${user.name}✦: 
+ 𝗕𝗔𝗟: ${user.money}💰`);
  
- const messageText = ` 𝗧𝗢𝗣 𝗥𝗘𝗖𝗛𝗘𝗦𝗧»🌐\n\n ${topUsersList.join('')}
+ const messageText = ` 𝗧𝗢𝗣 𝗥𝗘𝗖𝗛𝗘𝗦𝗧»🌐\n\n ${topUsersList.join('\n')}
 
-  𝗕𝗢𝗧»𝗔𝗗𝗠𝗜𝗡 𝗠𝗔𝗥𝗜𝗡𝗔 `;
+  𝗕𝗢𝗧»𝗔𝗗𝗠𝗜𝗡 𝗣𝗛𝗔𝗥𝗢𝗨𝗞 `;
 
  message.reply(messageText);
  }
